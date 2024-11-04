df494981182d8065da4e8f69fe96e2c05c75d06f
totallynobuzzismaloy <buzzispro@gmail.com>
2024-11-05 00:57:16 +0300

    Added 3 functions to structures.h

--- structures.h
+++ structures.h
@@ -40,15 +40,48 @@ void swapNodes(HuffmanNode** a, HuffmanNode** b) {
 }
 
 void insertMinHeap(MinHeap* minHeap, HuffmanNode* node) {
+	if(minHeap->size == minHeap->capacity) {
+		fprintf(stderr, "MinHeap is full, unable to insert new node\n");
+		return;
+	}
 
+	int i = minHeap->size;
+	minHeap->array[i] = node;
+	minHeap->size++;
+
+	while(i && minHeap->array[i]->frequency < minHeap->array[(i - 1) / 2]->frequency) {
+		swapNodes(&minHeap->array[i], &minHeap->array[(i - 1) / 2]);
+		i = (i - 1) / 2;
+	}
 }
 
 void minHeapify(MinHeap* minHeap, int index) {
+	int smallest = index;
+	int left = 2 * index + 1;
+	int right = 2 * index + 2;
 
+	if(left < minHeap->size && minHeap->array[left]->frequency < minHeap->array[smallest]->frequency)
+       		smallest = left;
+
+	if(right < minHeap->size && minHeap->array[right]->frequency < minHeap->array[smallest]->frequency)
+        	smallest = right;
+
+	if(smallest != index) {
+        	swapNodes(&minHeap->array[smallest], &minHeap->array[index]);
+        	minHeapify(minHeap, smallest);
+    	}
 }
 
 HuffmanNode* extractMin(MinHeap* minHeap) {
+	if(minHeap->size == 0) 
+		return NULL;
 
+	HuffmanNode* minNode = minHeap->array[0];
+	minHeap->array[0] = minHeap->array[minHeap->size - 1];
+	minHeap->size--;
+	minHeapify(minHeap, 0);
+
+	return minNode;
 }
 
 
