8ef6a83b7e3b47d763eff29d49388ace730b8b59
totallynobuzzismaloy <buzzispro@gmail.com>
2024-11-05 02:39:27 +0300

    Changing the logic which encodes the file

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ void freeTree(HuffmanNode* root);
 
 void printCodes(char codes[CHAR_COUNT][CHAR_COUNT], int frequency[]);
 void encodeFile(FILE *inFile, FILE *outFile, char codes[CHAR_COUNT][CHAR_COUNT]);
+void decodeFile(FILE* encodedFile, FILE* decodedFile, HuffmanNode* root);
 
 int main(int argc, char* argv[]) {
 	if(argc < 3) {
@@ -23,7 +24,7 @@ int main(int argc, char* argv[]) {
 	int frequency[CHAR_COUNT] = {0};
 	calculateFrequency(inputFile, frequency);
 	rewind(inputFile);
-	//fclose(inputFile);
+	fclose(inputFile);
 
 	printFrequencyTable(frequency);
 	
@@ -44,6 +45,15 @@ int main(int argc, char* argv[]) {
         	freeTree(root);
         	return 1;
     	}
+
+inputFile = fopen(argv[1], "r");
+if (inputFile == NULL) {
+    perror("Error reopening input file for encoding");
+    freeTree(root);
+    fclose(outputFile);
+    return 1;
+}
+
 
     	encodeFile(inputFile, outputFile, codes);
 
@@ -69,6 +79,21 @@ int main(int argc, char* argv[]) {
 
 	fclose(inputFile);
 	fclose(outputFile);
+
+	// Decoding section for debugging purposes
+    	FILE *encodedFile = fopen(argv[2], "rb");
+   	FILE *decodedFile = fopen("decoded_output.txt", "w");	
+
+	if(!encodedFile || !decodedFile) {
+		perror("Error opening file for decoding");
+        	freeTree(root);
+        	return 1;
+	}
+	decodeFile(encodedFile, decodedFile, root);	
+	printf("File successfully decoded to decoded_output.txt\n");	
+
+	fclose(encodedFile);
+    	fclose(decodedFile);
     	freeTree(root);	
 
 	return 0;
@@ -101,9 +126,54 @@ void printCodes(char codes[256][256], int frequency[]) {
 
 // Function to encode a text file using Huffman Codes
 void encodeFile(FILE *inFile, FILE *outFile, char codes[256][256]) {
-    char ch;
+ char ch;
+    unsigned char buffer = 0; // Buffer for bits
+    int bufferIndex = 0; // Number of bits currently in the buffer
+
     while ((ch = fgetc(inFile)) != EOF) {
-        fprintf(outFile, "%s", codes[(unsigned char)ch]);
+        char *code = codes[(unsigned char)ch]; // Get the Huffman code for the character
+        for (int i = 0; code[i] != '\0'; i++) {
+            // Add the bit to the buffer
+            buffer = (buffer << 1) | (code[i] - '0'); // '0' -> 0, '1' -> 1
+            bufferIndex++;
+
+            // If the buffer is full (8 bits), write it to the file
+            if (bufferIndex == 8) {
+                fputc(buffer, outFile);
+                buffer = 0; // Reset buffer
+                bufferIndex = 0;
+            }
+        }
+    }
+
+    // Handle any remaining bits in the buffer
+    if (bufferIndex > 0) {
+        buffer <<= (8 - bufferIndex); // Pad with zeros
+        fputc(buffer, outFile);
+    }
+}
+
+// Function to decode a file using the Huffman tree
+void decodeFile(FILE *encodedFile, FILE *decodedFile, HuffmanNode *root) {
+HuffmanNode *current = root;
+    int byte;
+
+    while ((byte = fgetc(encodedFile)) != EOF) {
+        for (int i = 7; i >= 0; i--) {
+            int bit = (byte >> i) & 1;
+
+            if (bit == 0) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+
+            // If at a leaf node, write the character
+            if (!current->left && !current->right) {
+                fputc(current->character, decodedFile);
+                current = root; // Reset for next character
+            }
+        }
     }
 }
 
