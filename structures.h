3b2320e1560c41f77b0063ae74d32a0ff539f48e
totallynobuzzismaloy <buzzispro@gmail.com>
2024-11-05 00:29:12 +0300

    Added functions to work with structures

--- structures.h
+++ structures.h
@@ -15,16 +15,28 @@ typedef struct MinHeap {
 
 } MinHeap;
 
-HuffmanNode* createNode(int capacity, int frequency) {
+HuffmanNode* createNode(char character, int frequency) {
+	HuffmanNode* newNode = (HuffmanNode*) malloc(sizeof(HuffmanNode));
+	newNode->character = character;
+	newNode->frequency = frequency;
+	newNode->left = newNode->right = NULL;
 
+	return newNode;
 }
 
 MinHeap* createMinHeap(int capacity) {
+	MinHeap* minHeap = (MinHeap*) malloc(sizeof(MinHeap));
+	minHeap->size = 0;
+	minHeap->capacity = capacity;
+	minHeap->array = (HuffmanNode**) malloc(capacity * sizeof(HuffmanNode*));
 
+	return minHeap;
 }
 
 void swapNodes(HuffmanNode** a, HuffmanNode** b) {
-
+	HuffmanNode *temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void insertMinHeap(MinHeap* minHeap, HuffmanNode* node) {
